2d5ed0be291c6ffc4ea7310dfddd9ac484394403
MonteBoomBox <manassjoshi@gmail.com>
2020-11-02 20:16:43 +0530

    Added Rocket & Reflectors
    Added 1st version sprites of Player Rocket and Reflectors.

--- Game Off Game Jam 2020/Assets/Scripts/Player.cs
+++ Game Off Game Jam 2020/Assets/Scripts/Player.cs
@@ -12,17 +12,25 @@ public class Player : MonoBehaviour
 
     public float boost = 200f;
 
+    private Vector2 ScreenBounds;
+
+    private bool isAlive;
+
+    public float DestroyDelay = 0.2f;
+
 
     void Start()
     {
+        isAlive = true;
         rb = GetComponent<Rigidbody2D>();
-        rb.isKinematic = true;        
+        rb.isKinematic = true;
+        ScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
         movement = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetKey(KeyCode.Space))
@@ -39,5 +47,11 @@ public class Player : MonoBehaviour
     public void FixedUpdate()
     {
         transform.RotateAround(transform.position, Vector3.forward, movement * Time.fixedDeltaTime * -rotateSpeed);
+    }
+
+    public void OnBecameInvisible()
+    {
+        isAlive = false;
+        Destroy(gameObject, DestroyDelay);
     }
 }
