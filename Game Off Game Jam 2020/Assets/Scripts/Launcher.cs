7cae769a0aacadbe2fb0828a327ccf821e601812
MonteBoomBox <manassjoshi@gmail.com>
2020-11-02 15:30:11 +0530

    Rapid Prototype v1.0
    1. Added Rocket Movement
    2. Added Launcher Movement
    3. Add Bouncy Platforms

---
+++ Game Off Game Jam 2020/Assets/Scripts/Launcher.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    public float moveSpeed = 200f;
+
+    private float movement = 0f;
+
+    public float launchSpeed = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LauncherMovement();
+        movement = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    public void LauncherMovement()
+    {
+        Vector2 CannonPos = transform.position;
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePos - CannonPos;
+        transform.right = direction;
+    }
+
+    public void Shoot()
+    {
+        GameObject Rocket = GameObject.FindGameObjectWithTag("Rocket");
+        Rigidbody2D rb = Rocket.GetComponent<Rigidbody2D>();
+        Rocket.transform.parent = null;
+        rb.isKinematic = false;
+        rb.velocity = transform.up * launchSpeed;
+        gameObject.GetComponent<Launcher>().enabled = false;
+    }
+}
---
+++ Game Off Game Jam 2020/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float movement = 0f;
+
+    public float rotateSpeed = 150f;
+
+    Rigidbody2D rb;
+
+    public float boost = 200f;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        movement = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ApplyThrust();
+        }
+    }
+
+    public void ApplyThrust()
+    {
+        rb.AddRelativeForce(Vector3.up * boost * Time.deltaTime);
+    }
+
+    public void FixedUpdate()
+    {
+        transform.RotateAround(transform.position, Vector3.forward, movement * Time.fixedDeltaTime * -rotateSpeed);
+    }
+}
